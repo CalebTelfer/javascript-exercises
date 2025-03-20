853c9ce9f895493ab3f010e50e9fb38e003df2c3
caleb <calebtelfe@gmail.com>
2025-03-20 15:28:08 -0400

    03 done.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(enteredString) {
+
+    let reversedString = "";
+    let stringLength = enteredString.length;
+
+    for (let i = stringLength; i >= 0; --i) {
+        console.log(i)
+        reversedString += enteredString.charAt(i);
+    }
+
+    return reversedString;
 
 };
 
