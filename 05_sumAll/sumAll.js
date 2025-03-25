0dc63b88085e6d1b66c2d4efb5b7ab00252005ed
caleb <calebtelfe@gmail.com>
2025-03-25 14:31:50 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+        return "ERROR";
+    }
+
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+
+    //prevents numbers w decimals
+    if (numOne % 1 != 0 || numTwo % 1 != 0) {
+        return "ERROR";
+    }
+
+    let lowNum, highNum = 0;
+
+    if (numOne < numTwo) {
+        lowNum = numOne;
+        highNum = numTwo;
+    } else {
+        lowNum = numTwo;
+        highNum = numOne;
+    }
+
+    let sum = 0;
+    for (let i = lowNum; i <= highNum; i++) {
+        sum = sum + i;
+        console.log(sum);
+    }
+
+    return sum;
+
 
 };
 
