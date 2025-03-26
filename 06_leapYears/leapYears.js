54ea15467cf76acdacab780bc51637ae85847f8b
caleb <calebtelfe@gmail.com>
2025-03-26 13:42:50 -0400

    06 finished.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+//leap years are divisible by 4, but years divisble by 100 are not leap years unless divisble by 400 aswell.
+
+const leapYears = function(year) {
+    if(year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 != 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
