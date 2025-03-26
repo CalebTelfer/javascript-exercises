2a128bb7a1f8967dd3bff36a6da03873942679ea
caleb <calebtelfe@gmail.com>
2025-03-26 14:00:11 -0400

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  // farenheit to celsius =  temp - 32 divided by 1.8
+  const convertedTemp = (inputTemp - 32) / 1.8;
+  return Math.round(convertedTemp * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(inputTemp) {
+  // celsius to farenheit = temp * 1.8 plus 32 after
+  const convertedTemp = (inputTemp * 1.8) + 32;
+  return Math.round(convertedTemp * 10) / 10;
 
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
