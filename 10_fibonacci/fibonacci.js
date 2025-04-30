03cb4ba209bbc10c03732b82bf17671b03929981
caleb <calebtelfe@gmail.com>
2025-04-30 14:23:44 -0400

    10 done. Javascript excersise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+// 0, 1, 1, 2, 3, 5, 8 <--- fibonacci example. two preceding numbers make the next number
+
+if (n == 0) {   // first value of fibonacci is 0 so return 0 if its 0
+    return 0;
+
+} else if (n == 1) { // second value of fibonacci is 1 so return 1.
+    return 1;
+
+}
+
+if (n < 0) { return "OOPS"};
+
+return fibonacci(n-1) + fibonacci(n-2);
+
+// ^ using the two known values of 0, and 1, to recursively work our way up to the fibonacci
+// number that we requested when we originally called the function.
 
 };
 
